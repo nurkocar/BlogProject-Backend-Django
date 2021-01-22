3f0570352b946bec41b21787f2607eeb1bf4c037
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2021-01-22 17:08:48 +0000

    Created CategoryList view

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Category, Recipe, Ingredient, Comment, Like, RecipeView
+from .serializers import CategorySerializer, RecipeSerializer, RecipeDetailSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 
 # Create your views here.
+class CategoryList(generics.ListAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+    
