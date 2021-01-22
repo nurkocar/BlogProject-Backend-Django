2a4eff0149ecd5734fa22d0a8e03dd6cf501a8c8
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2021-01-22 19:17:25 +0000

    Completed blog urls.py

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import CategoryList, RecipeList, RecipeDetail
 
-
-# urlpatterns = [
-#     path('',)
-# ]
+urlpatterns = [
+    path("", CategoryList.as_view(), name="categoryList"),
+    path("<category>/recipeList", RecipeList.as_view(), name="recipeList"),
+    path("recipe/<title>/", RecipeDetail.as_view(), name="recipeDetail")
+]
