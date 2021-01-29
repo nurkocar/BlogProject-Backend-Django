6e0c259d752349e8991b90c94a08ce346dd8e844
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2021-01-29 18:44:10 +0000

    Created RegisterView and Profile_get and update views

--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from blog import serializers
+from rest_framework import status
+from .models import Profile
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import UserSerializer, ProfileSerializer, RegisterSerializer
 
 # Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    
+    
+@api_view(['GET', 'PUT'])
+def Profile_get_update(request):
+    if request.method == 'GET':
+        serializer = ProfileSerializer(request.user.profile)
+        
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        serializer = ProfileSerializer(request.user.profile, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            
+            data = {
+                'message': 'Profile is updated successfully!'
+            }
+            
+            return Response(data)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
