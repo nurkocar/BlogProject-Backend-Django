83c4946b05d242cf6f6d44abb11da507d8b2193c
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2021-01-29 16:53:37 +0000

    Created ProfileSerializer and RegisterSerializer

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -13,3 +13,54 @@ class UserSerializer(serializers.ModelSerializer):
         'email'
     )
     
+
+class ProfileSerializer(serializers.ModelSerializer):
+    
+    user = serializers.StringRelatedField()
+    
+    fields = (
+        'user',
+        'image',
+        'bio'
+    )
+    
+class RegisterSerializer(serializers.ModelSerializer):
+    
+    email = serializers.EmailField(
+        required=True, validators=[UniqueValidator(queryset = User.objects.all())]
+    )
+    
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password], style={'input_type' : 'password'}
+    )
+    
+    password2 = serializers.CharField(
+        write_only=True, required=True, style={'input_type' : 'password'}
+    )
+    
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email',
+            'password',
+            'password2 '
+        )
+        
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {'password': 'Passwords did not match.'}
+            )
+        return attrs
+    
+    def create(self, validated_data):
+        user = User.objects.create(
+            username = validate_data['username'],
+            email = validated_data['email']
+        )
+        
+        user.set_password(validated_data['password'])
+        user.save()
+        
+        return user
